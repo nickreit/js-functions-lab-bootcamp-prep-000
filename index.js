f9fbe70885e4a3d3069510a2983654b842bec12b
nickreit@icloud.com <nickreit@icloud.com>
2018-08-29 19:21:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function happyHolidaysTo(name) {
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
